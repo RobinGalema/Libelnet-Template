33b8e5cd19aab594050b5aeb4cb32c1a5e2b384e
Robin Galema <RGalwma99@gmail.com>
2020-12-08 10:36:59 +0100

    Update to version 2.1.1
    Changelog:
    (-) test-page.php -> dit bestand is niet nodig in het template, de bieb wordt nu gebruikt om nieuwe onderdelen te testen
    (-) Fout geplaatste foto in de root van het thema
    
    (+) Bootstrap classes in archive.php om lay-out te fixen
    (+) Link naar de homepage vanuit de 404 pagina
    
    ~ scss include bestanden verplaatst naar een map buiten de "scss" naar het mapje "scss-includes" in "assets" om te voorkomen dat scss bestanden meerdere keren worden ingeladen.
    ~ Layout van de 404 pagina aangepast om te passen bij het standaard systeem

--- libelnet-template-theme/404.php
+++ libelnet-template-theme/404.php
@@ -7,12 +7,17 @@ get_header();
 ?>
 
     <main id="primary" class="site-main">
-
-        <section class="error-404 not-found">
-            <h2>404 - Page not Found</h2>
-        </section><!-- .error-404 -->
-
-    </main><!-- #main -->
+        <section id="404">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                        <h2>404 - Page not Found</h2>
+                        <a href="<?php echo home_url(); ?>">Terug naar de homepage</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
 <?php
 get_footer();
